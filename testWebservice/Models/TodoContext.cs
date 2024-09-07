5fefe2220d0ab6a52315e69ca8ac561e0023e76b
4R4S8 <4R4S8@EYZEE>
2024-09-07 19:37:41 +0330

    todo API

--- testWebservice/Models/TodoContext.cs
+++ testWebservice/Models/TodoContext.cs
@@ -1,6 +1,14 @@
-﻿namespace testWebservice.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace testWebservice.Models
 {
-    public class TodoContext
+    public class TodoContext : DbContext
     {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
     }
 }
